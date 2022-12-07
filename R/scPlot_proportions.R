444f2d0432b1bbf58e553e96cfc0b17099621806
damouzo <daniel.mouzo@unavarra.es>
2022-12-07 11:01:32 +0100

    First upload

---
+++ R/scPlot_proportions.R
@@ -0,0 +1,63 @@
+
+# Proportions ident in scRNA plot ---------------------------------------------
+scPlot_proportions = function (seurat_obj, by_ident, clusters=F,
+                               PlotNcells=T, value_reorder=T) {
+  ## take an Seurat object, plot distributions over orig.ident
+  library(Seurat)
+  library(patchwork)
+  library(ggplot2)
+  library(reshape2)
+  library(BuenColors)
+
+  seurat_obj@meta.data$Sample_Name <- vapply(strsplit(as.character(seurat_obj@meta.data$orig.ident),"_"), `[`, 3, FUN.VALUE=character(1))
+
+  count_table <- table(seurat_obj@meta.data[[by_ident]], seurat_obj@meta.data$Sample_Name)
+  count_mtx   <- as.data.frame.matrix(count_table)
+  count_mtx$cluster <- rownames(count_mtx)
+  melt_mtx    <- melt(count_mtx)
+  melt_mtx$cluster <- as.factor(melt_mtx$cluster)
+
+  cluster_size   <- aggregate(value ~ cluster, data = melt_mtx, FUN = sum)
+
+  if (clusters == T) {
+    sorted_labels <- paste(sort(as.integer(levels(cluster_size$cluster)),decreasing = T))
+  } else {
+    sorted_labels <- paste(sort(levels(cluster_size$cluster),decreasing = T))
+  }
+
+  cluster_size$cluster <- factor(cluster_size$cluster,levels = sorted_labels)
+  melt_mtx$cluster <- factor(melt_mtx$cluster,levels = sorted_labels)
+  melt_mtx$tot_Ncells <-ave(melt_mtx$value, melt_mtx$variable, FUN=sum)
+  melt_mtx$percent <- melt_mtx$value*100/melt_mtx$tot_Ncells
+  colnames(melt_mtx)[2] <- "Dataset"
+
+  # Plots per se
+  if (value_reorder == T) {
+    #Plot total proportion in Log scale.
+    p1 <- ggplot(cluster_size, aes(x = value,y= reorder(cluster, value)))
+    #Plot Barplot by ident
+    p2 <- ggplot(melt_mtx,aes(x=reorder(cluster, percent),y=percent,fill=Dataset))
+  } else {
+    #Plot total proportion in Log scale.
+    p1 <- ggplot(cluster_size, aes(x = value, y= cluster))
+    #Plot Barplot by ident
+    p2 <- ggplot(melt_mtx,aes(x=cluster,y=percent,fill=Dataset))
+  }
+
+  my_pal <- c(jdb_palette("corona"),jdb_palette("brewer_spectra"),jdb_palette("lawhoops"),
+              jdb_palette("samba_color"),jdb_palette("wolfgang_basic"))
+
+  p1 <- p1 + geom_bar(position="dodge", stat="identity",fill = "grey60") +
+    theme_bw() + scale_x_log10() + xlab("Cells per ident, log10 scale") +
+    {if(PlotNcells) geom_text(aes(y=cluster,x=1.1,label=value,hjust="bottom"), size=2.5)} + ylab("")
+
+  p2 <- p2 + geom_bar(position="dodge", stat="identity") + theme_bw() + coord_flip() +
+    {if(PlotNcells) geom_text(aes(cluster,y = 0.1,label=value, group=Dataset, hjust =0), position=position_dodge(width=1), size=2.5)} +
+    scale_fill_manual(values = my_pal) +
+    ylab("Percentage of cells in each dataset") + xlab("Idents") + theme(legend.position="top")
+
+  #Sum plots
+  p2 + p1 + plot_layout(widths = c(3,1))
+}
+
+
---
+++ R/scPlot_treedot.R
@@ -0,0 +1,167 @@
+
+
+# Plot TreeDot for GSEA comparision results -----------------------------------
+scPlot_treedot <- function(comp_pair_term, top_paths=5, clust_num=3, ORA_ont="BP",
+                         ORA_minGSSize=10, ORA_maxGGSSize=500, ORA_p.adj=1){
+  # libraries
+  library(ggplot2)
+  library(tidyverse)
+  library(ggdendro)
+  library(cowplot)
+  library(clusterProfiler)
+  library(ggtree)
+  library(patchwork)
+  library(org.Hs.eg.db)
+  library(RColorBrewer)
+
+  # Estruture Data
+  comp_pair_term_fort <- fortify(comp_pair_term, showCategory= top_paths,
+                                 includeAll = TRUE, split = NULL)
+  comp_pair_term_fort$Cluster <- sub("\n.*", "", comp_pair_term_fort$Cluster)
+  comp_pair_term_fort$geneID <- comp_pair_term_fort$core_enrichment
+
+  # Merge Clusters
+  merge_compareClusterResult <- function(yy) {
+    yy_union <- yy[!duplicated(yy$ID),]
+    yy_ids <- lapply(split(yy, yy$ID), function(x) {
+      ids <- unique(unlist(strsplit(x$geneID, "/")))
+      cnt <- length(ids)
+      list(ID=paste0(ids, collapse="/"), cnt=cnt)
+    })
+
+    ids <- vapply(yy_ids, function(x) x$ID, character(1))
+    cnt <- vapply(yy_ids, function(x) x$cnt, numeric(1))
+
+    yy_union$geneID <- ids[yy_union$ID]
+    yy_union$Count <- cnt[yy_union$ID]
+    yy_union$Cluster <- NULL
+    yy_union
+  }
+  merged_ggData <- merge_compareClusterResult(comp_pair_term_fort)
+
+  # Prepare data for IDs Cluster
+  prepare_pie_category <- function(enrichDf, pie = "equal") {
+    pie <- match.arg(pie, c("equal", "count", "Count"))
+    if (pie == "count") pie <- "Count"
+
+    pie_data <- enrichDf[,c("Cluster", "Description", "Count")]
+    pie_data[,"Description"] <- as.character(pie_data[,"Description"])
+    prepare_pie_data(pie_data, pie = pie)
+  }
+  prepare_pie_data <- function(pie_data, pie = "equal",type = "category") {
+    if(type == "category"){
+      ID_unique <- unique(pie_data[,2])
+    } else {
+      ID_unique <- unique(pie_data[,3])
+    }
+
+    Cluster_unique <- unique(pie_data[,1])
+    ID_Cluster_mat <- matrix(0, nrow = length(ID_unique), ncol = length(Cluster_unique))
+    rownames(ID_Cluster_mat) <- ID_unique
+    colnames(ID_Cluster_mat) <- Cluster_unique
+    ID_Cluster_mat <- as.data.frame(ID_Cluster_mat, stringAsFactors = FALSE)
+    if(pie == "Count") {
+      for(i in seq_len(nrow(pie_data))) {
+        ID_Cluster_mat[pie_data[i,2],pie_data[i,1]] <- pie_data[i,3]
+      }
+      for(kk in seq_len(ncol(ID_Cluster_mat))) {
+        ID_Cluster_mat[,kk] <- as.numeric(ID_Cluster_mat[,kk])
+      }
+      return(ID_Cluster_mat)
+    }
+    for(i in seq_len(nrow(pie_data))) {
+      if(type == "category"){
+        ID_Cluster_mat[pie_data[i,2],pie_data[i,1]] <- 1
+      } else {
+        ID_Cluster_mat[pie_data[i,3],pie_data[i,1]] <- 1
+      }
+
+    }
+    return(ID_Cluster_mat)
+  }
+  ID_Cluster_mat <- prepare_pie_category(comp_pair_term_fort,pie = "equal")
+
+  # Hierarchical Clustering
+  fill_termsim <- function(x, keep) {
+    termsim <- x@termsim[keep, keep]
+    termsim[which(is.na(termsim))] <- 0
+    termsim2 <- termsim + t(termsim)
+    for ( i in seq_len(nrow(termsim2)))
+      termsim2[i, i] <- 1
+    return(termsim2)
+  }
+  termsim2 <- fill_termsim(comp_pair_term, rownames(ID_Cluster_mat))
+  hc <- stats::hclust(stats::as.dist(1- termsim2),method = "ward.D")
+
+  # Info of Clusters wanted
+  clus <- stats::cutree(hc, clust_num)
+
+  # ORA for clusters
+  keywords <- vector()
+  for (i in 1:clust_num) {
+    paths_of_clust <- clus[clus == i]
+
+    cluster_paths_info <- comp_pair_term_fort[comp_pair_term_fort$Description %in% names(paths_of_clust) ,]
+    genes2check <-  unique(unlist(str_split(cluster_paths_info$geneID, "/")))
+
+
+    if (is.null(comp_pair_term@.call$keyType) == F ){
+      keytype_called <- comp_pair_term@.call$keyType
+    } else if (is.null(comp_pair_term@.call$keyType) == T) {
+      paste0("Warning: object@.call$keyType is empty, asumming ENTREZID,
+            is and error ocurr please fill keytype argument in compareCluster() ")
+      keytype_called <- "ENTREZID"
+    }
+
+
+    ora <- enrichGO(genes2check, OrgDb= "org.Hs.eg.db", keyType = keytype_called,
+                    ont = ORA_ont, pvalueCutoff = ORA_p.adj, pAdjustMethod = "BH",
+                    qvalueCutoff = 1,minGSSize = ORA_minGSSize,maxGSSize = ORA_maxGGSSize)
+
+    keywords[i] <- ora@result$Description[1]
+  }
+
+
+  # Plot dendogram
+  g <- split(names(clus), clus)
+  p <- ggtree(hc, size=1.2)
+  clades <- sapply(g, function(n) MRCA(p, n))
+  p <- groupClade(p, clades, group_name='SubTree_ORA') + aes(color=SubTree_ORA)
+  mycolors = c(brewer.pal(name="Dark2", n = 8), brewer.pal(name="Set1", n = 6))
+  ggtree_plot <- p + scale_color_manual(values=mycolors, breaks=1:clust_num,labels = keywords)+
+    theme(legend.position='right',legend.justification = c(0,1.5))
+  ggtree_plot_noLegend <- ggtree_plot + theme(legend.position = "none")
+
+
+  # Prepare for dotplot
+  comp_pair_term_fort$log_p.adjust <- -log10(comp_pair_term_fort$p.adjust)
+  comp_pair_term_fort$Description = factor(comp_pair_term_fort$Description,
+                                           levels = hc$labels[hc$order])
+  comp_pair_term_fort$Cluster <- factor(comp_pair_term_fort$Cluster,
+                                        levels=levels(comp_pair_term@compareClusterResult$Cluster))
+
+  # Plot dotplot
+  dotplot <- comp_pair_term_fort %>%
+    ggplot(aes(x=Cluster, y =Description , color = NES, size = log_p.adjust)) +
+    geom_point() +
+    scale_y_discrete(position = "right")+
+    scale_color_gradient2(low="blue4",mid="white", high="red")+
+    cowplot::theme_cowplot() +
+    theme(axis.line  = element_blank()) +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+    ylab('') +
+    guides(size=guide_legend(title="-log10(p.adj)"))+
+    theme(axis.ticks = element_blank(),legend.position = "right",legend.justification = c(0,0))
+  dotplot_noLegend <- dotplot + theme(legend.position = "none")
+
+  # Legends
+  legend_tree <- get_legend(ggtree_plot)
+  legend_dot <- get_legend(dotplot)
+
+  # Merge Plot
+  combine_plot <- plot_grid(ggtree_plot_noLegend,NULL, dotplot_noLegend, nrow= 1, rel_widths= c(0.3,-0.05,2), align = 'h')
+  combine_legend <- plot_grid(legend_dot,NULL,legend_tree, ncol=1, rel_heights = c(1,-0.5,1))
+  big_plot <- plot_grid(combine_plot,combine_legend,NULL, nrow = 1, rel_widths = c(1,0.1, 0.16))
+  return(big_plot)
+}
+
